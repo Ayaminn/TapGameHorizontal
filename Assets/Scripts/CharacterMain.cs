798c2c470389803fea802229f609fcf3a951accd
Ayaminn <ayamin.imai@gmail.com>
2016-02-05 17:36:00 +0900

    First Commit

---
+++ Assets/Scripts/CharacterMain.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMain : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float jump;
+	Camera _2DCamera;
+	Camera right3DCamera;
+	Camera left3DCamera;
+	public bool leftCamera3D = false;
+	public bool rightCamera3D = false;
+	public bool camera2D = true;
+	public bool onGround = false;
+	Coll coll;
+
+	// Use this for initialization
+	void Start () {
+		//coll = transform.findchild ("On").GetComponent<Coll> ().nowOn;
+
+		rb = GetComponent<Rigidbody> ();
+
+		_2DCamera = GameObject.Find("2D Camera").GetComponent<Camera>();
+		right3DCamera = GameObject.Find ("3D Camera Right").GetComponent<Camera> ();
+		left3DCamera = GameObject.Find ("3D Camera Left").GetComponent<Camera> ();
+		right3DCamera.enabled = false;
+		left3DCamera.enabled = false;
+	}
+
+	void OnCollisionEnter(Collision col){
+		onGround = true;
+	}
+
+	void OnCollisionExit(Collision col){
+		onGround = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			//カメラ切り替え
+			if (_2DCamera.enabled) {
+				_2DCamera.enabled = false;
+				right3DCamera.enabled = true;
+				right3DCamera.transform.eulerAngles = new Vector3 (0, 90, 0);
+				rightCamera3D = true;
+				leftCamera3D = false;
+				camera2D = false;
+			} else {
+				_2DCamera.enabled = true;
+				right3DCamera.enabled = false;
+				rightCamera3D = false;
+				leftCamera3D = false;
+				camera2D = true;
+			}
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			if (_2DCamera.enabled) {
+				_2DCamera.enabled = false;
+				left3DCamera.enabled = true;
+				left3DCamera.transform.eulerAngles = new Vector3 (0, -90, 0);
+				rightCamera3D = false;
+				leftCamera3D = true;
+				camera2D = false;
+			} else {
+				_2DCamera.enabled = true;
+				left3DCamera.enabled = false;
+				rightCamera3D = false;
+				leftCamera3D = false;
+				camera2D = true;
+			}
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if (camera2D == true){
+				transform.position += new Vector3 (0.1f, 0, 0);
+			} else if (rightCamera3D == true) {
+				transform.position += new Vector3 (0, 0, -0.1f);
+			} else {
+				transform.position += new Vector3 (0, 0, 0.1f);
+			}
+
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (camera2D == true){
+				transform.position += new Vector3 (-0.1f, 0, 0);
+			} else if (rightCamera3D == true) {
+				transform.position += new Vector3 (0, 0, 0.1f);
+			} else {
+				transform.position += new Vector3 (0, 0, -0.1f);
+			}
+
+		} else if (Input.GetKeyDown(KeyCode.Space) && onGround) {
+			//ジャンプ
+			if(rb.velocity.y < 0.2f) {
+				rb.AddForce(transform.up * jump);
+			}
+
+		} else if (Input.GetKey(KeyCode.UpArrow) && camera2D == false) {
+			if (rightCamera3D == true) {
+				transform.position += new Vector3 (0.1f, 0, 0);
+			} else {
+				transform.position += new Vector3 (-0.1f, 0, 0);
+			}
+		} else if (Input.GetKey(KeyCode.DownArrow) && camera2D == false) {
+			if (rightCamera3D == true) {
+				transform.position += new Vector3 (-0.1f, 0, 0);
+			} else {
+				transform.position += new Vector3 (0.1f, 0, 0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Coll.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coll : MonoBehaviour {
+
+	public bool nowOn;
+}
