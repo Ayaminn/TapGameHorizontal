ac7d3b4bf2c8dc3cce3c8014c46dddccc63c1ee1
Ayaminn <ayamin.imai@gmail.com>
2016-03-09 19:53:25 +0900

    (5)完璧〜

---
+++ Assets/Scripts/Five.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Five : MonoBehaviour {
+	Renderer renderer;
+	Camera _2DCamera;
+	Camera right3DCamera;
+	Camera left3DCamera;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+		renderer.enabled = false;
+		_2DCamera = GameObject.Find("2D Camera").GetComponent<Camera>();
+		right3DCamera = GameObject.Find ("3D Camera Right").GetComponent<Camera> ();
+		left3DCamera = GameObject.Find ("3D Camera Left").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_2DCamera.enabled == true) {
+			renderer.enabled = false;
+		} else {
+			renderer.enabled = true;
+		}
+	}
+}
