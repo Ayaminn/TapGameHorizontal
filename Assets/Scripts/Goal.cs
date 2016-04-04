b7c9b01d61c31e01f7ff49ef5a9d25e8c395c576
Ayaminn <ayamin.imai@gmail.com>
2016-04-04 21:07:16 +0900

    ゴール途中まで

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class Goal : MonoBehaviour {
 	public Camera came2;
 
+	private bool isMainColor = false;
+	[SerializeField] Color color1 = Color.white, color2 = Color.white;
+	[SerializeField] UnityEngine.UI.Image image = null;
+
+	[SerializeField]
+	CanvasGroup group = null;
+
+	[SerializeField]
+	Fade fade = null;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -21,7 +31,17 @@ public class Goal : MonoBehaviour {
 
 	void OnCollisionEnter (Collision col) {
 		if (col.transform.tag == "Player") {
-			Application.LoadLevel ("Goal");
+			group.blocksRaycasts = false;
+			fade.FadeIn (1, () =>
+				{
+					//image.color = (isMainColor) ? color1 : color2;
+					isMainColor = !isMainColor;
+					Application.LoadLevel ("Goal");
+					fade.FadeOut(1, ()=>{
+						
+						group.blocksRaycasts = true;
+					});
+				});
 		}
 	}
 }
